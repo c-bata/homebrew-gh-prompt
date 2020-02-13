80979f3c9bc8c70d241d0c360608169a0f0d4c5f
c-bata <contact@c-bata.link>
2020-02-13 20:38:37 +0900

    init

---
+++ gh-prompt.rb
@@ -0,0 +1,12 @@
+class KubePrompt < Formula
+  desc "An interactive GitHub CLI featuring auto-complete."
+  homepage "https://github.com/c-bata/gh-prompt"
+  url "https://github.com/c-bata/gh-prompt/releases/download/v0.0.1/gh-prompt_darwin_x86_64.zip"
+  sha256 "0b9982834119001a7eb7881789361f2aeffc41de7c2ae1dea92873498c14a067"
+  version "0.0.1"
+
+  def install
+    bin.install "gh-prompt"
+  end
+end
+
