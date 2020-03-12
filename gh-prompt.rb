3205ea0eab7b44acf025ee90419c7e8c78e6d8fc
c-bata <contact@c-bata.link>
2020-03-12 16:13:13 +0900

    Release v0.0.2

--- gh-prompt.rb
+++ gh-prompt.rb
@@ -1,9 +1,9 @@
 class GhPrompt < Formula
   desc "An interactive GitHub CLI featuring auto-complete."
   homepage "https://github.com/c-bata/gh-prompt"
-  url "https://github.com/c-bata/gh-prompt/releases/download/v0.0.1/gh-prompt_darwin_x86_64.zip"
-  sha256 "0b9982834119001a7eb7881789361f2aeffc41de7c2ae1dea92873498c14a067"
-  version "0.0.1"
+  url "https://github.com/c-bata/gh-prompt/releases/download/v0.0.2/gh-prompt_darwin_x86_64.zip"
+  sha256 "da0b5db8748ea95cb19535f899b4ca397e4c844056da66d2c1ade8e2cc59010"
+  version "0.0.2"
 
   def install
     bin.install "gh-prompt"
